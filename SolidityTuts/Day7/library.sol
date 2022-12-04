d7e77b5791333f26a5cfa934663a46cbb79183cb
deep0072 <deepakumar50136@gmail.com>
2022-12-04 18:23:26 +0530

    library defined

---
+++ SolidityTuts/Day7/library.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.6;
+
+library RemoveArray {
+    // these dont use state variable
+
+    function remove(uint[] storage arr, uint index) public {
+        require(arr.length > 0, "cant remove from array");
+        arr[index] = arr[arr.length - 1];
+        arr.pop();
+    }
+}
+
+contract TestArray {
+    // calling library here
+    using RemoveArray for uint[];
+    uint[] public arr;
+
+    function getArray() public returns (uint[] memory) {
+        for (uint i = 0; i < 5; i++) {
+            arr.push(i);
+        }
+        arr.remove(1); // .remove function defined in library is called
+
+        return arr;
+    }
+}
