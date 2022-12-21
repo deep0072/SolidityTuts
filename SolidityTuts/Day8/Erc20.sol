f21601c4f1aa08a4e28969005da58c9f061f7448
deep072 <deepakumar50136@gmail.com>
2022-12-22 01:25:41 +0530

    buy function added to buy token  using ether

--- SolidityTuts/Day8/Erc20.sol
+++ SolidityTuts/Day8/Erc20.sol
@@ -4,9 +4,47 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Erc20Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("DeepCoin", "DC") {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ICO  is ERC20 , Ownable {
+    constructor() ERC20("DeepCoin", "DC") {
         // ERC20() initiating parent constructor with name and symbol of coin
-        _mint(msg.sender, initialSupply); // create new token using _mint() function
+        _mint(msg.sender, 1000000*(10**uint256(decimals()))); // create new token using _mint() function
     }
+
+    function mint(address account, uint256 amount) public onlyOwner returns(bool){
+        require(account != address(this) && amount != uint256(0), "mint invalid");
+        _mint(account, amount);
+        return true;
+    }
+
+    function Burn(address _add, uint _amt) public onlyOwner returns(bool){
+        require (_add != address(this) && _amt != uint256(0), "mint invalid");
+        _burn(_add,_amt);
+    }
+
+    function withDraw(uint256 _amt) public onlyOwner returns(bool){
+        require(_amt <= address(this).balance, "ICO , withdraw is not available");
+        _transfer(owner(), _msgSender(), _amt);
+    }
+
+    // buy erc20 token using ether. in one ether we will get 1000 token
+    function Buy() public payable returns(bool){
+        require(msg.value <= address(this).balance && msg.value !=0, "not enough balance");
+
+        uint amount = msg.value * 1000;
+
+        _transfer(owner(), _msgSender(), amount);
+        return (true);
+
+
+
+    }
+
+
+   
 }
+
+
+
+
