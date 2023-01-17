7f268ea996f1f22c14cb4ab66d62c3351b5366ba
deep072 <deepakumar50136@gmail.com>
2023-01-17 08:06:01 +0530

    gas report config file added

--- SolidityTuts/Day9/multisigHardhat/hardhat.config.js
+++ SolidityTuts/Day9/multisigHardhat/hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  gasReporter: {
+    enabled: true,
+    currency: "INR",
+    noColors: true,
+    outputFile: "gasReports.txt",
+    coinmarketcap: "4e1796d9-7d73-41cb-871c-42189bfed03c",
+    token: "metic",
+  },
 };
